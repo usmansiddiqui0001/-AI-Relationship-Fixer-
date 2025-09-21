aa708debbb6680fbbd0e9f907307304a5d42cfc6
usmansiddiqui0001 <gpt348996@gmail.com>
2025-09-21 11:10:34 -0700

    Fixing vite config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/<YOUR-REPO-NAME>/', // Replace <YOUR-REPO-NAME> with your repository name
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+    plugins: [react()],
+    base: '/', // Set base path for Vercel deployment
+  }
 })
